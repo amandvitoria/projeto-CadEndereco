2b1d4b2ab373f196daa5c31219835108020f1073
Amnada vitoria <amandvitoria244@gmail.com>
2023-09-29 10:05:53 -0300

    preenchendo JS

--- controller.js
+++ controller.js
@@ -1,15 +1,23 @@
 'use restrict'; //Modo restrito
 
- 
-
 //Limpar formulário
 const LimparFormulario = (endereco) =>{
     document.getElementById('rua').value = '';
-    document.getElementById('estado').value = '';
     document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
 
 }
 
 //Verifica se CEP é válido
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+//preenche campos do formulario 
+const preencheFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+
+}
